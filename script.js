cf39baa649bcc76e7c3911cef09d078e3d8c154a
ad-lo <102575296+ad-lo@users.noreply.github.com>
2022-09-06 05:19:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'ugtdQAz1gYCabCQFsUk2RLofjyl1WmNy9UCrAtCPCSY';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all images were loaded
+function imageloaded (){
+imagesLoaded++;
+console.log(imagesLoaded);
+if (imageloaded === totalImages) {
+  ready = true;
+  loader.hidden = true;
+  console.log('ready =', ready);
+
+}
+}
 
 // Helper function to set attributes on DOM elements 
 function setAttributes(element, attributes) {
@@ -17,6 +32,9 @@ function setAttributes(element, attributes) {
 
 //  Create Elements for links & photos, add to DOM
 function displayPhotos() {
+  imageloaded = 0;
+  totalImages = photosArray.length;
+  console.log('totalImages', totalImages);
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
 //  create <a> to link to unsplash
@@ -34,6 +52,8 @@ setAttributes(img, {
   alt: photo.alt_description,
   title: photo.alt_description
 });
+// event listener, check when each is finished loading
+img.addEventListener('load', imageloaded); 
 // put <img> inside <a>, then put both inside image container element
 item.appendChild(img);
 imageContainer.appendChild(item);
@@ -51,6 +71,14 @@ displayPhotos();
     // catch error here
   }
 }
+
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+ ready = false;
+  getPhotos();
+}
+});
 
 // On load
 getPhotos();
